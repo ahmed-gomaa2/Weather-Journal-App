d3dc7160b4d98872541c6044604b56f9b27b2510
ahmed-gomaa2 <engahmedgomaa97@gmail.com>
2020-12-07 02:18:01 -0800

    finishing up the app

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App is listening to port: ${port}`)
+})
+
+app.post('/sendweather', (req, res) => {
+    projectData.temp = req.body.data.main.temp;
+    projectData.date = req.body.date;
+    projectData.content = req.body.content;
+
+    res.send(projectData)
+})
+
+app.get('/all', (req, res) => {
+    res.send(projectData)
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,78 @@
 /* Global Variables */
+const apiKey = '31a296fb035ec451688ba5b3d21a57d7';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
 
-// Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const btn = document.getElementById('generate');
+
+const getData = async (url='', zip, key) => {
+    const response = await fetch(`${url}?zip=${zip}&appid=${key}&units=metric`);
+    try{
+        const res = await response.json();
+        return res;
+    }catch(err) {
+        console.log(err)
+    }
+}
+
+const sendData = async (url='', data={}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'content-type': 'application/JSON',
+        },
+        body: JSON.stringify(data)
+    });
+}
+
+const retrieveData = async (url='') => {
+    const response = await fetch(url);
+    
+    try {
+        const res = await response.json();
+        return res;
+    }catch (err) {
+        console.log(err)
+    }
+}
+
+const updateUI = (data) => {
+    let image;
+    if(data.temp < 25) {
+        image = 'https://images.unsplash.com/photo-1457269449834-928af64c684d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c25vd3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60';
+    }else if(data.temp >= 25 && data.temp < 33) {
+        image = 'https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8c3ByaW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60';
+    }else if(data.temp > 35) {
+        image = 'https://images.unsplash.com/photo-1517384084767-6bc118943770?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8c3VtbWVyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60';
+    }
+    document.getElementById('date').innerHTML = `Date: ${data.date}`;
+    document.getElementById('temp').innerHTML = `Temperature: ${data.temp} ℃`;
+    document.getElementById('app').style.backgroundImage = `url(${image})`
+    if(data.content) {
+        document.getElementById('content').innerHTML = `Content: ${data.content}`;
+    }
+}
+
+const callTheFetchDataFun = () => {
+    const zip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+    if(!zip) {
+        alert("Enter Your US zip code!")
+    }else {
+        getData(baseURL,zip, apiKey).then(data => {
+            console.log(data)
+            const object = {data: data, date: newDate, content: content};
+            sendData('/sendweather', object).then(() => {
+                retrieveData('/all').then(retrievedData => {
+                    updateUI(retrievedData);
+                })
+            })
+        });
+    }
+}
+
+btn.addEventListener('click', callTheFetchDataFun);
+
